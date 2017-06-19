ff458a1bb714199df4fed784e21f19f6ca6b003e
HelloBiao <1537130693@qq.com>
2017-06-19 13:31:46 +0800

    修改定时器执行

--- camel-demo/src/main/java/com/xiaoka/camel/timer/TimerDemo.java
+++ camel-demo/src/main/java/com/xiaoka/camel/timer/TimerDemo.java
@@ -1,10 +1,38 @@
 package com.xiaoka.camel.timer;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.model.ModelCamelContext;
+
 public class TimerDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-
+		ModelCamelContext camelContext = new DefaultCamelContext();
+		camelContext.start();
+		
+		camelContext.addRoutes(new RouteBuilder() {
+			
+			@Override
+			public void configure() throws Exception {
+				// TODO Auto-generated method stub
+				from("timer:foo?period=2000").process(new Processor() {
+					
+					public void process(Exchange exchange) throws Exception {
+						// TODO Auto-generated method stub
+						System.out.println("exchangeid : "+exchange.getExchangeId());
+					}
+				});
+			}
+		});
+		//防止程序退出
+		synchronized (TimerDemo.class) {
+			TimerDemo.class.wait();
+		}
+		
 	}
 
 }
